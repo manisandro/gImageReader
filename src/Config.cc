b4f6f5ea5a43fe304196e26dc334128d3fa02bc7
Sandro Mani <manisandro@gmail.com>
2013-05-01 17:32:17 +0200

    Revert to using language code followed by country code, else breaks dictionary installation

--- src/Config.cc
+++ src/Config.cc
@@ -32,48 +32,48 @@ static void dictDescribe(const char* const tag, const char* const /*providerName
 
 const std::vector<Config::Lang> Config::LANGUAGES = {
 	// {ISO 639-2, ISO 639-1, name}
-	{"ara", "ar", "\u0627\u0644\u0639\u0631\u0628\u064a\u0629"},
-	{"bul", "bg", "\u0431\u044a\u043b\u0433\u0430\u0440\u0441\u043a\u0438 \u0435\u0437\u0438\u043a"},
-	{"cat", "ca", "Catal\u00e0"},
-	{"ces", "cs", "\u010cesky"},
-	{"chi_sim", "zh", "\u7b80\u4f53\u5b57"},
-	{"chi_tra", "zh", "\u7c21\u9ad4\u5b57"},
-	{"dan-frak", "da", "Dansk (Frak)"},
-	{"dan", "da", "Dansk"},
-	{"deu-frak", "de", "Deutsch (Frak)"},
-	{"deu-f", "de", "Deutsch (Frak)"},
-	{"deu", "de", "Deutsch"},
-	{"ell", "el", "\u0395\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac"},
-	{"eng", "en", "English"},
-	{"fin", "fi", "Suomi"},
-	{"fra", "fr", "Fran\u00e7ais"},
-	{"heb", "he", "\u05e2\u05d1\u05e8\u05d9\u05ea"},
-	{"hrv", "hr", "hrvatski"},
-	{"hun", "hu", "Magyar"},
-	{"ind", "id", "Bahasa Indonesia"},
-	{"ita", "it", "Italiano"},
-	{"jpn", "ja", "\u65e5\u672c\u8a9e"},
-	{"kan", "kn", "\u0c95\u0ca8\u0ccd\u0ca8\u0ca1"},
-	{"kor", "ko", "\ud55c\uad6d\ub9d0"},
-	{"lav", "lv", "Latvie\u0161u Valoda"},
-	{"lit", "lt", "Lietuvi\u0173 Kalba"},
-	{"nld", "nl", "Nederlands"},
-	{"nor", "no", "Norsk"},
-	{"pol", "pl", "Polski"},
-	{"por", "pt", "Portugu\u00eas"},
-	{"ron", "ro", "Rom\u00e2n\u0103"},
-	{"rus", "ru", "P\u0443\u0441\u0441\u043a\u0438\u0439 \u044f\u0437\u044b\u043a"},
-	{"slk-frak", "sk", "Sloven\u010dina"},
-	{"slk", "sk", "Sloven\u010dina"},
-	{"slv", "sl", "Sloven\u0161\u010dina"},
-	{"spa", "es", "Espa\u00f1ol"},
-	{"srp", "sr", "\u0441\u0440\u043f\u0441\u043a\u0438 \u0458\u0435\u0437\u0438\u043a"},
-	{"swe-frak", "sv", "Svenska (Frak)"},
-	{"swe", "sv", "Svenska"},
-	{"tgl", "tl", "Wikang Tagalog"},
-	{"tur", "tr", "T\u00fcrk\u00e7e"},
-	{"ukr", "uk", "Y\u043a\u0440\u0430\u0457\u043d\u0441\u044c\u043a\u0430"},
-	{"vie", "vi", "Ti\u1ebfng Vi\u1ec7t"}
+	{"ara", "ar_AE", "\u0627\u0644\u0639\u0631\u0628\u064a\u0629"},
+	{"bul", "bg_BG", "\u0431\u044a\u043b\u0433\u0430\u0440\u0441\u043a\u0438 \u0435\u0437\u0438\u043a"},
+	{"cat", "ca_ES", "Catal\u00e0"},
+	{"ces", "cs_CS", "\u010cesky"},
+	{"chi_sim", "zh_CN", "\u7b80\u4f53\u5b57"},
+	{"chi_tra", "zh_CN", "\u7c21\u9ad4\u5b57"},
+	{"dan-frak", "da_DK", "Dansk (Frak)"},
+	{"dan", "da_DK", "Dansk"},
+	{"deu-frak", "de_DE", "Deutsch (Frak)"},
+	{"deu-f", "de_DE", "Deutsch (Frak)"},
+	{"deu", "de_DE", "Deutsch"},
+	{"ell", "el_GR", "\u0395\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac"},
+	{"eng", "en_US", "English"},
+	{"fin", "fi_FI", "Suomi"},
+	{"fra", "fr_FR", "Fran\u00e7ais"},
+	{"heb", "he_IL", "\u05e2\u05d1\u05e8\u05d9\u05ea"},
+	{"hrv", "hr_HR", "hrvatski"},
+	{"hun", "hu_HU", "Magyar"},
+	{"ind", "id_ID", "Bahasa Indonesia"},
+	{"ita", "it_IT", "Italiano"},
+	{"jpn", "ja_JP", "\u65e5\u672c\u8a9e"},
+	{"kan", "kn_IN", "\u0c95\u0ca8\u0ccd\u0ca8\u0ca1"},
+	{"kor", "ko_KR", "\ud55c\uad6d\ub9d0"},
+	{"lav", "lv_LV", "Latvie\u0161u Valoda"},
+	{"lit", "lt_LT", "Lietuvi\u0173 Kalba"},
+	{"nld", "nl_NL", "Nederlands"},
+	{"nor", "no_NO", "Norsk"},
+	{"pol", "pl_PL", "Polski"},
+	{"por", "pt_PT", "Portugu\u00eas"},
+	{"ron", "ro_RO", "Rom\u00e2n\u0103"},
+	{"rus", "ru_RU", "P\u0443\u0441\u0441\u043a\u0438\u0439 \u044f\u0437\u044b\u043a"},
+	{"slk-frak", "sk_SK", "Sloven\u010dina"},
+	{"slk", "sk_SK", "Sloven\u010dina"},
+	{"slv", "sl_SI", "Sloven\u0161\u010dina"},
+	{"spa", "es_ES", "Espa\u00f1ol"},
+	{"srp", "sr_RS", "\u0441\u0440\u043f\u0441\u043a\u0438 \u0458\u0435\u0437\u0438\u043a"},
+	{"swe-frak", "sv_SE", "Svenska (Frak)"},
+	{"swe", "sv_SE", "Svenska"},
+	{"tgl", "tl_PH", "Wikang Tagalog"},
+	{"tur", "tr_TR", "T\u00fcrk\u00e7e"},
+	{"ukr", "uk_UA", "Y\u043a\u0440\u0430\u0457\u043d\u0441\u044c\u043a\u0430"},
+	{"vie", "vi_VN", "Ti\u1ebfng Vi\u1ec7t"}
 };
 
 Config::Config()
@@ -94,7 +94,7 @@ Config::Config()
 		view->set_model(Gtk::ListStore::create(m_langViewCols));
 		CONNECT(view, size_allocate, [this,view](Gtk::Allocation& a){ viewResizeCols(view, a); });
 		view->append_column(_("Filename prefix"), m_langViewCols.prefix);
-		view->append_column(_("ISO 639-1 code"), m_langViewCols.code);
+		view->append_column(_("Code"), m_langViewCols.code);
 		view->append_column(_("Native name"), m_langViewCols.name);
 		for(int i=0; i<3; ++i){
 			view->get_column(i)->set_sizing(Gtk::TREE_VIEW_COLUMN_FIXED);
@@ -170,6 +170,7 @@ bool Config::searchLangSpec(const Glib::RefPtr<Gtk::TreeModel> model, Lang& lang
 void Config::updateLanguagesMenu()
 {
 	m_langsMenu.forall([this](Gtk::Widget& w){ m_langsMenu.remove(w); });
+	m_curlang = Lang();
 	m_radioGroup = Gtk::RadioButtonGroup();
 	Gtk::RadioMenuItem* radioitem = nullptr;
 	Gtk::RadioMenuItem* activeradio = nullptr;
@@ -188,10 +189,8 @@ void Config::updateLanguagesMenu()
 	if(availLanguages.empty()){
 		Utils::error_dialog(_("No languages available"), _("No tesseract languages are available for use. Recognition will not work."));
 		m_langLabel->set_text("");
-		Builder("tbmenu:main.recognize").as<Gtk::Widget>()->set_sensitive(false);
 		return;
 	}
-	Builder("tbmenu:main.recognize").as<Gtk::Widget>()->set_sensitive(true);
 
 	for(int i = 0; i < availLanguages.size(); ++i){
 		Lang lang = {availLanguages[i].string()};
@@ -201,7 +200,7 @@ void Config::updateLanguagesMenu()
 		std::vector<Glib::ustring> spelldicts;
 		if(!lang.code.empty()){
 			for(const Glib::ustring& dict : dicts){
-				if(dict.substr(0, 2) == lang.code){
+				if(dict.substr(0, 2) == lang.code.substr(0, 2)){
 					spelldicts.push_back(dict);
 				}
 			}
@@ -218,7 +217,7 @@ void Config::updateLanguagesMenu()
 				Glib::ustring prettyname = lang.name + " (" + dict + ")";
 				CONNECT(radioitem, toggled, [this, radioitem, itemlang, prettyname]{ setLanguage(radioitem, itemlang, prettyname); });
 				if((curlang.prefix == lang.prefix) &&
-				   (curlang.code.empty() || curlang.code == dict.substr(0, 2) || curlang.code == dict))
+				   (curlang.code.empty() || curlang.code.substr(0, 2) == dict.substr(0, 2) || curlang.code == dict))
 				{
 					curlang = itemlang;
 					radioitem->set_active(true);
@@ -304,7 +303,7 @@ void Config::addLanguage()
 		invalid = true;
 		Utils::set_error_state(m_addLangName);
 	}
-	if(!Glib::Regex::create("^[a-z]{2}$")->match(m_addLangCode->get_text())){
+	if(!Glib::Regex::create("^[a-z]{2}_[A-Z]{2}$")->match(m_addLangCode->get_text())){
 		invalid = true;
 		Utils::set_error_state(m_addLangCode);
 	}
