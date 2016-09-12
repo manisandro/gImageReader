55e2e94b65525930c6853ed86043ec27d6890039
Sandro Mani <manisandro@gmail.com>
2016-09-12 12:01:11 +0200

    Revert "Use HTML5 doctype declaration in hocr file"
    This reverts commit 48d26419ec76a0ec1f6ee217f91137412ee1bc54.

--- gtk/src/OutputEditorHOCR.cc
+++ gtk/src/OutputEditorHOCR.cc
@@ -990,14 +990,14 @@ bool OutputEditorHOCR::save(const std::string& filename)
 	}
 	tesseract::TessBaseAPI tess;
 	Glib::ustring header = Glib::ustring::compose(
-			"<!DOCTYPE html>\n"
-			"<html>\n"
+			"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
+			"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n"
 			" <head>\n"
 			"  <title></title>\n"
-			"  <meta charset=\"UTF-8\">\n"
-			"  <meta name='ocr-system' content='tesseract %1' />\n"
-			"  <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocrx_word'/>\n"
-			" </head>\n"
+			"  <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />\n"
+			"    <meta name='ocr-system' content='tesseract %1' />\n"
+			"    <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocrx_word'/>\n"
+			"  </head>\n"
 			"<body>\n", tess.Version());
 	file.write(header.data(), std::strlen(header.data()));
 	for(Gtk::TreeIter item : m_itemStore->children()) {
