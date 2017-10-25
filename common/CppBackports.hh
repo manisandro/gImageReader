978fe050eff89688800fc678be46725d2e847c12
Alexander Zaitsev <zamazan4ik@tut.by>
2017-10-25 14:38:09 +0300

    Add make_unique implementation

--- common/CppBackports.hh
+++ common/CppBackports.hh
@@ -1,8 +1,34 @@
-//
-// Created by zamazan4ik on 25.10.17.
-//
+/* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * Acquirer.hh
+ * Copyright (C) 2017 Alexander Zaitsev <zamazan4ik@tut.by>
+ *
+ * gImageReader is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gImageReader is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef GIMAGEREADER_CPPBACKPORTS_HH
 #define GIMAGEREADER_CPPBACKPORTS_HH
+
+#include <memory>
+
+namespace gstd
+{
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+}
 
 #endif //GIMAGEREADER_CPPBACKPORTS_HH
