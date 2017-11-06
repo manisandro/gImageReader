02f209449a2d154cc3fe705d3cecef2bff8881f3
Alexander Zaitsev <zamazan4ik@tut.by>
2017-11-06 17:14:25 +0300

    [Qt] Add feature: opening encrypted PDF
    Refactor code
    
    
    Refactor and fix comment
    
    
    Disable messagebox
    
    
    Change text on the input dialog
    
    
    Add missing colon character

--- qt/src/SourceManager.hh
+++ qt/src/SourceManager.hh
@@ -30,10 +30,10 @@ class UI_MainWindow;
 
 
 struct Source {
-	Source(const QString& _path, const QString& _displayname, bool _isTemp = false)
-		: path(_path), displayname(_displayname), isTemp(_isTemp) {}
+	Source(const QString& _path, const QString& _displayname, bool _isTemp = false);
 	QString path;
 	QString displayname;
+	QByteArray password;
 	bool isTemp;
 	int brightness = 0;
 	int contrast = 0;
