6414bd7e91654258ef982f58ed322c1f144e7a18
Sandro Mani <manisandro@gmail.com>
2017-12-08 20:11:13 +0100

    [Qt] Add HOCR text exporter (#236)

---
+++ qt/src/hocr/HOCRTextExporter.cc
@@ -0,0 +1,83 @@
+/* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * HOCRTextExporter.cc
+ * Copyright (C) 2013-2017 Sandro Mani <manisandro@gmail.com>
+ *
+ * gImageReader is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gImageReader is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Config.hh"
+#include "HOCRDocument.hh"
+#include "HOCRTextExporter.hh"
+#include "MainWindow.hh"
+#include "FileDialogs.hh"
+#include "SourceManager.hh"
+
+#include <QFileInfo>
+#include <QMessageBox>
+#include <QTextStream>
+
+bool HOCRTextExporter::run(const HOCRDocument *hocrdocument, QString &filebasename)
+{
+	QString suggestion = filebasename;
+	if(suggestion.isEmpty()) {
+		QList<Source*> sources = MAIN->getSourceManager()->getSelectedSources();
+		suggestion = !sources.isEmpty() ? QFileInfo(sources.first()->displayname).baseName() : _("output");
+	}
+
+	QString outname = FileDialogs::saveDialog(_("Save Text Output..."), suggestion + ".txt", "outputdir", QString("%1 (*.txt)").arg(_("Text Files")));
+	if(outname.isEmpty()) {
+		return false;
+	}
+	filebasename = QFileInfo(outname).completeBaseName();
+
+	QFile outputFile(outname);
+	if(!outputFile.open(QIODevice::WriteOnly)) {
+		QMessageBox::warning(MAIN, _("Export failed"), _("The text export failed: unable to write output file."));
+		return false;
+	}
+
+	QString output;
+	QTextStream outputStream(&output, QIODevice::WriteOnly);
+	for(int i = 0, n = hocrdocument->pageCount(); i < n; ++i) {
+		const HOCRPage* page = hocrdocument->page(i);
+		if(!page->isEnabled()) {
+			continue;
+		}
+		printItem(outputStream, page);
+	}
+	outputFile.write(MAIN->getConfig()->useUtf8() ? output.toUtf8() : output.toLocal8Bit());
+	return true;
+}
+
+void HOCRTextExporter::printItem(QTextStream& outputStream, const HOCRItem* item, bool lastChild)
+{
+	if(!item->isEnabled()) {
+		return;
+	}
+	QString itemClass = item->itemClass();
+	if(itemClass == "ocrx_word") {
+		outputStream << item->text();
+		if(!lastChild) {
+			outputStream << " ";
+		}
+	} else {
+		for(int i = 0, n = item->children().size(); i < n; ++i) {
+			printItem(outputStream, item->children()[i], i == n - 1);
+		}
+	}
+	if(itemClass == "ocr_line" || itemClass == "ocr_par") {
+		outputStream << "\n";
+	}
+}
--- qt/src/hocr/Ui_OutputEditorHOCR.hh
+++ qt/src/hocr/Ui_OutputEditorHOCR.hh
@@ -19,9 +19,11 @@
 
 class UI_OutputEditorHOCR {
 public:
+	QToolButton* toolButtonOutputExport;
 	QAction* actionOutputOpen;
 	QAction* actionOutputClear;
 	QAction* actionOutputSaveHOCR;
+	QAction* actionOutputExportText;
 	QAction* actionOutputExportPDF;
 	QAction* actionToggleWConf;
 	QAction* actionPick;
@@ -44,9 +46,20 @@ public:
 		actionOutputSaveHOCR = new QAction(QIcon::fromTheme("document-save-as"), gettext("Save as hOCR text"), widget);
 		actionOutputSaveHOCR->setToolTip(gettext("Save as hOCR text"));
 		actionOutputSaveHOCR->setEnabled(false);
+		QMenu* exportMenu = new QMenu();
+		actionOutputExportText = new QAction(QIcon::fromTheme("text-plain"), gettext("Export to plain text"), widget);
+		actionOutputExportText->setToolTip(gettext("Export to plain text"));
+		exportMenu->addAction(actionOutputExportText);
 		actionOutputExportPDF = new QAction(QIcon::fromTheme("application-pdf"), gettext("Export to PDF"), widget);
 		actionOutputExportPDF->setToolTip(gettext("Export to PDF"));
-		actionOutputExportPDF->setEnabled(false);
+		exportMenu->addAction(actionOutputExportPDF);
+		toolButtonOutputExport = new QToolButton(widget);
+		toolButtonOutputExport->setIcon(QIcon::fromTheme("document-export"));
+		toolButtonOutputExport->setText(gettext("Export"));
+		toolButtonOutputExport->setToolTip(gettext("Export"));
+		toolButtonOutputExport->setEnabled(false);
+		toolButtonOutputExport->setMenu(exportMenu);
+		toolButtonOutputExport->setPopupMode(QToolButton::InstantPopup);
 		actionOutputClear = new QAction(QIcon::fromTheme("edit-clear"), gettext("Clear output"), widget);
 		actionOutputClear->setToolTip(gettext("Clear output"));
 		actionToggleWConf = new QAction(QIcon(":/icons/wconf"), gettext("Show confidence values"), widget);
@@ -58,7 +71,7 @@ public:
 		toolBarOutput->setIconSize(QSize(1, 1) * toolBarOutput->style()->pixelMetric(QStyle::PM_SmallIconSize));
 		toolBarOutput->addAction(actionOutputOpen);
 		toolBarOutput->addAction(actionOutputSaveHOCR);
-		toolBarOutput->addAction(actionOutputExportPDF);
+		toolBarOutput->addWidget(toolButtonOutputExport);
 		toolBarOutput->addAction(actionOutputClear);
 		toolBarOutput->addSeparator();
 		toolBarOutput->addAction(actionToggleWConf);
