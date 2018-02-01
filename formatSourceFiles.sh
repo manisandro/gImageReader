5fb50517c8c7c36ffcfec8675b4a40338ff92952
Sandro Mani <manisandro@gmail.com>
2018-02-01 23:16:45 +0100

    Allow formatSourceFiles.sh to be used as a git pre-commit hook

--- formatSourceFiles.sh
+++ formatSourceFiles.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 dir=$(dirname "$(readlink -f "$0")")
-which astyle &> /dev/null || echo "Please install astyle." && exit 1
-find gtk qt \( -name '*.cc' -or -name '*.hh' \) -exec astyle --options=$dir/astyle.options {} \;
+which astyle &> /dev/null || ( echo "Please install astyle." && exit 1 )
+
+out=$(astyle --formatted --options=$dir/astyle.options $(find gtk qt \( -name '*.cc' -or -name '*.hh' \)))
+echo "$out"
+[ -z "$out" ] && exit 0 || exit 1
