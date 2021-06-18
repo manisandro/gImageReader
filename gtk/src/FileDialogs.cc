6901ba77c296503978646c0c904323b96d7aab1b
zeev <dummy@mail.com>
2021-06-18 18:42:31 +0300

    fix type

--- gtk/src/FileDialogs.cc
+++ gtk/src/FileDialogs.cc
@@ -124,7 +124,7 @@ static std::string win32_open_folder_dialog(const Glib::ustring& title, const st
 		// ToDo: shouldn't we release the memory hold by "result"?...
 		return std::string();
 	}
-	wchar_t buffer[MAX_PATH];
+	TCHAR buffer[MAX_PATH];
 	if(SHGetPathFromIDList(result, buffer)) {
 	    // ToDo: shouldn't we release the memory hold by "result"?...
 		return std::string(buffer);
