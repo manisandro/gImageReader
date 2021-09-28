f848c5a5e3614c03e932fcd8c1d6916ba8d90dc5
Tadahito Yao <eighttails@nifty.com>
2021-09-28 19:10:46 +0900

    [Qt] Apply adaptive Otsu thresholding instead of normal Otsu. (#547)
    It makes better binarization result.

--- qt/src/Recognizer.cc
+++ qt/src/Recognizer.cc
@@ -204,6 +204,9 @@ std::unique_ptr<tesseract::TessBaseAPI> Recognizer::setupTesseract() {
 		tess->SetPageSegMode(MAIN->getRecognitionMenu()->getPageSegmentationMode());
 		tess->SetVariable("tessedit_char_whitelist", MAIN->getRecognitionMenu()->getCharacterWhitelist().toLocal8Bit());
 		tess->SetVariable("tessedit_char_blacklist", MAIN->getRecognitionMenu()->getCharacterBlacklist().toLocal8Bit());
+#if TESSERACT_VERSION >= TESSERACT_MAKE_VERSION(5, 0, 0)
+		tess->SetVariable("thresholding_method", "1");
+#endif
 	} else {
 		QMessageBox::critical(MAIN, _("Recognition errors occurred"), _("Failed to initialize tesseract"));
 	}
