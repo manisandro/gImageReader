5e00d3e595885953b0442827bede07eda0676763
Sandro Mani <manisandro@gmail.com>
2025-04-10 09:24:08 +0200

    [Qt] Only use QCheckBox::checkStateChanged on Qt >= 6.7.0

--- qt/src/hocr/OutputEditorHOCR.cc
+++ qt/src/hocr/OutputEditorHOCR.cc
@@ -156,7 +156,11 @@ HOCRAttributeCheckbox::HOCRAttributeCheckbox(Qt::CheckState value, HOCRDocument*
 	: m_doc(doc), m_itemIndex(itemIndex), m_attrName(attrName), m_attrItemClass(attrItemClass) {
 	setCheckState(value);
 	connect(m_doc, &HOCRDocument::itemAttributeChanged, this, &HOCRAttributeCheckbox::updateValue);
+#if QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
 	connect(this, &HOCRAttributeCheckbox::checkStateChanged, this, &HOCRAttributeCheckbox::valueChanged);
+#else
+	connect(this, &HOCRAttributeCheckbox::stateChanged, this, &HOCRAttributeCheckbox::valueChanged);
+#endif
 }
 
 void HOCRAttributeCheckbox::updateValue(const QModelIndex& itemIndex, const QString& name, const QString& value) {
